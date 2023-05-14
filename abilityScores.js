b0a47dbe02aaf636973f3afb5f6786df793a56d6
Austin Hunt <austin.hunt.012@gmail.com>
2023-05-13 23:13:39 -0500

    updated function to set new limits and scores at the same time

--- abilityScores.js
+++ abilityScores.js
@@ -1,6 +1,6 @@
 //Creating a function to set ability scores.
 
-function processStats(AddRemove, inType, NameEntity, inScoresA, dialogTxt, isSpecial, inAlsoHasMax, maxIsLimitToNow){
+function processStats(AddRemove, inType, NameEntity, inScoresA, dialogTxt, isSpecial, inAlsoHasMax, maxIsLimitToNow) {
     let scoresA = inScoresA && isArray(inScoresA) ? [].concat(inScoresA) : [];
     let alsoHasMax = inAlsoHasMax && isArray(inAlsoHasMax) ? [].concat(inAlsoHasMax) : false;
     initiateCurrentStats();
@@ -16,16 +16,16 @@ function processStats(AddRemove, inType, NameEntity, inScoresA, dialogTxt, isSpe
             break;
         }
     }
-    if (!curStat && type ==="background") {
+    if (!curStat && type === "background") {
         ASaddColumn("Backgr-\nound", true);
         i = CurrentStats.cols.length - 1;
         curStat = CurrentStats.cols[i];
     } else if (!curStat) {
         return;
     }
-    let  imprTxtArr = [], saveMaximumsLimited;
+    let imprTxtArr = [], saveMaximumsLimited;
     //this will allow the changes to be set to the total variable//
-    for (let s = 0; s < scoresA.length; s++){
+    for (let s = 0; s < scoresA.length; s++) {
         if (AddRemove && maxIsLimitToNow && alsoHasMax && scoresA[s] && alsoHasMax[s]) {
             //don't let it go above the maximum listed
             /* Only add the bonus up to the maximum listed, or less if possible
@@ -45,13 +45,13 @@ function processStats(AddRemove, inType, NameEntity, inScoresA, dialogTxt, isSpe
             alsoHasMax = [].concat(CurrentStats.maximumsLimited[NameEntity]);
             saveMaximumsLimited = "remove";
         }
-        if (type ==="race") curStat.scores[s] = 0;
+        if (type === "race") curStat.scores[s] = 0;
         if (!scoresA[s]) continue;
-        if (AddRemove && !dialogTxt && s <7 ) {
+        if (AddRemove && !dialogTxt && s < 7) {
             let theScoreName = s < 6 ? AbilityScores.names[s] : What("HoSRememberState");
-            let theScore = type == "override" ? theScoreName+ " is " + scoresA[s]
-                :type === "maximum" ? theScoreName + " maximum is " + scoresA[s]
-                    : (scoresA[s] > 0 ? "+" : "") +scoresA[s] + " " + theScoreName;
+            let theScore = type == "override" ? theScoreName + " is " + scoresA[s]
+                : type === "maximum" ? theScoreName + " maximum is " + scoresA[s]
+                    : (scoresA[s] > 0 ? "+" : "") + scoresA[s] + " " + theScoreName;
             imprTxtArr.push(theScore);
         }
         if (isSpecial) {
@@ -61,18 +61,18 @@ function processStats(AddRemove, inType, NameEntity, inScoresA, dialogTxt, isSpe
                 delete CurrentStats[isSpecial][s][NameEntity];
             }
             //------------------------------------This is setting the highest value for override
-            curStat.scores[s]=0;
+            curStat.scores[s] = 0;
             let aMods = [];
             for (let a in CurrentStats[isSpecial][s]) {
                 let thisStat = CurrentStats[isSpecial][s][a];
-                if (isNaN(thisStat.substring(0,1)) && !isNaN(thisStat.substring(1))){
+                if (isNaN(thisStat.substring(0, 1)) && !isNaN(thisStat.substring(1))) {
                     aMods.push(thisStat);
                 } else if (!isNaN(thisStat) && thisStat > curStat.scores[s]) {
                     curStat.scores[s] = Number(thisStat);
                 }
             }
-           if (type === "maximum" && !curStat.scores[s]) curStat.scores[s] =20;
-           if (aMods.length) curStat.scores[s] = processModifiers(curStat.scores[s], aMods);
+            if (type === "maximum" && !curStat.scores[s]) curStat.scores[s] = 20;
+            if (aMods.length) curStat.scores[s] = processModifiers(curStat.scores[s], aMods);
         } else {
             if (AddRemove) {
                 curStat.scores[s] += scoresA[s];
@@ -81,33 +81,46 @@ function processStats(AddRemove, inType, NameEntity, inScoresA, dialogTxt, isSpe
             }
         }
     }
-if (saveMaximumsLimited === "save") {
-    if (!CurrentStats.maximumsLimited) CurrentStats.maximumsLimited = {};
-    CurrentStats.maximumsLimited[NameEntity] = alsoHasMax;
-} else if (saveMaximumsLimited === "remove") {
-    delete CurrentStats.maximumsLimited[NameEntity];
-}
+    if (saveMaximumsLimited === "save") {
+        if (!CurrentStats.maximumsLimited) CurrentStats.maximumsLimited = {};
+        CurrentStats.maximumsLimited[NameEntity] = alsoHasMax;
+    } else if (saveMaximumsLimited === "remove") {
+        delete CurrentStats.maximumsLimited[NameEntity];
+    }
 //------------------------------------------------------------set up a flag if there is no maximum set
 
-    if (alsoHasMax && alsoHasMax.reduce(function(acc, val) { return acc + val; }, 0) === 0) alsoHasMax = false;
+    if (alsoHasMax && alsoHasMax.reduce(function (acc, val) {
+        return acc + val;
+    }, 0) === 0) alsoHasMax = false;
 //    sets description text.
-if (AddRemove) {
-    let useDialogTxt = dialogTxt ? dialogTxt : formatLineList("", imprTxtArr);
-    //adds dialog if it doesnt exist yet.
-    if (!CurrentStats.txts[inType][NameEntity] || CurrentStats.txts[inType][NameEntity].indexOf(useDialogTxt) === -1) {
-        CurrentStats.txts[inType][NameEntity] = (!dialogTxt && CurrentStats.txts[inType][NameEntity] ? CurrentStats.txts[inType][NameEntity] + "; " : "") + useDialogTxt;
-    } else if (!dialogTxt && !alsoHasMax && CurrentStats.txts[inType][NameEntity].indexOf(useDialogTxt) !== -1) {
-        // If the entry already exists and contains the exact text that we are about to add, which isn't predetermined, skip it entirely
-        CurrentStats = eval(What("CurrentStats.Stringified"));
-        return;
+    if (AddRemove) {
+        let useDialogTxt = dialogTxt ? dialogTxt : formatLineList("", imprTxtArr);
+        //adds dialog if it doesnt exist yet.
+        if (!CurrentStats.txts[inType][NameEntity] || CurrentStats.txts[inType][NameEntity].indexOf(useDialogTxt) === -1) {
+            CurrentStats.txts[inType][NameEntity] = (!dialogTxt && CurrentStats.txts[inType][NameEntity] ? CurrentStats.txts[inType][NameEntity] + "; " : "") + useDialogTxt;
+        } else if (!dialogTxt && !alsoHasMax && CurrentStats.txts[inType][NameEntity].indexOf(useDialogTxt) !== -1) {
+            // If the entry already exists and contains the exact text that we are about to add, which isn't predetermined, skip it entirely
+            CurrentStats = eval(What("CurrentStats.Stringified"));
+            return;
+        }
+    } else {
+        delete CurrentStats.txts[inType][NameEntity];
+        if (type === "background" && !ObjLength(CurrentStats.txt.background) && Number(curStat.scores.join("")) === 0) {
+            CurrentStats.cols.splice(i, 1);
+        }
     }
-} else {
-    delete CurrentStats.txts[inType][NameEntity];
-    if( type === "background" && !ObjLength(CurrentStats.txt.background) && Number(curStat.scores.join("")) === 0 ){
-        CurrentStats.cols.splice(i, 1);
-    }
-}
 // Store the ability score change for changing the max limit and the score at the same time
+    if (alsoHasMax) {
+        CurrentStats.maximumsLimited[NameEntity] = scoresA;
+    } else {
+        delete CurrentStats.maximumsLimited[NameEntity];
+    }
+    processStats(AddRemove, inType, NameEntity, alsoHasMax, dialogTxt, "maximums", false);
+}
 
-
+else
+{
+    SetStringifieds("stats");
+    CurrentUpdates.types.push("stats" + inType);
+}
 }
